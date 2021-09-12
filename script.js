4050968361a7859352f1dd71a3f800e397596b56
mamiano202 <denza1591@msn.com>
2021-09-12 16:28:43 +0200

    version12sept

---
+++ script.js
@@ -0,0 +1,15 @@
+const tag = document.querySelectorAll(".tag a");
+function colorTag() {
+    tag.style.color = "blue";
+  }
+
+  <article aria-label="Photographer Profil" class="ph-profil">
+                <div class="ph-infos">
+                    <h2>Mimi Keel</h2>
+                    <p class="ph-city">London, UK</p>
+                    <p class="ph-tagline">Voir le beau dans le quotidien</p>
+                    <p><a class="ph-tags" href="index.html">#portrait</a> <a class="ph-tags" href="index.html">#events</a> <a class="ph-tags" href="index.html">#travel</a> <a class="ph-tags" href="index.html">#animals</a></p>
+                </div>
+                <button id="ph-contact" title="Contact Me">Contactez-moi</button>
+                <a href="#" title="Mimi Keel portrait"><img src="medias/portrait/MimiKeel.jpg" alt="Mimi Keel portrait"></a>
+            </article>
