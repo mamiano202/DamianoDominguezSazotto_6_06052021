ad7322fb3edee18751567a77e008c8f100038cc5
mamiano202 <denza1591@msn.com>
2021-11-25 17:36:04 +0100

    25night

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-
+let allPhotographers;
 const section = document.querySelector("#Photographers");
 
-var requestURL = "data.json";
+const requestURL = "data.json";
 
-var request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
 
 request.open("GET", requestURL);
 
@@ -11,29 +11,54 @@ request.responseType = "json";
 request.send();
 
 request.onload = function () {
-  var photographers = request.response;
-  showPhotograph(photographers);
+  allPhotographers = request.response["photographers"];
+  addTags();
+  const filter = window.location.hash.slice(1);
+  showPhotograph(filter);
 };
 
-function populateHeader(jsonObj) {
-  var myPara = document.createElement("p");
+function addTags(){
+  let content = "";
+  let listOfTags = [];
+  allPhotographers.forEach(photographer=>{
+    listOfTags = listOfTags.concat(photographer.tags);
+  });
+  const list =  [...new Set(listOfTags)];
+  list.forEach(tag=>{
+    content += /*html */`<a href="#${tag}" onclick="showPhotograph('${tag}')">${"#"+tag}</a>`;
+  });
+  document.querySelector("nav.filters").innerHTML = content;
 }
 
-function showPhotograph(jsonObj) {
-  var photographer = jsonObj["photographers"];
+// function populateHeader() {
+//   var myPara = document.createElement("p");
+// }
 
-  for (var i = 0; i < photographer.length; i++) {
+function filteredByTag(filter){
+  if (filter === "") return allPhotographers;
+
+  const list = [];
+  allPhotographers.forEach(photographer => {
+    if (photographer.tags.indexOf(filter) > -1) list.push(photographer);
+  });
+  return list;
+}
+
+function showPhotograph(filter) {
+  section.innerText="";
+  const photographers = filteredByTag(filter);
+  for (var i = 0; i < photographers.length; i++) {
     var myArticle = document.createElement("div");
     myArticle.classList.add("cards")
     myArticle.innerHTML = /*html*/ `
         
-        <a  href="./photographer.html?id=${photographer[i].id}">
-            <img src="Sample Photos/Photographers ID Photos/${photographer[i].portrait}" alt="${photographer[i].name}">
+        <a  href="./photographer.html?id=${photographers[i].id}">
+            <img src="Sample Photos/Photographers ID Photos/${photographers[i].portrait}" alt="${photographers[i].name}">
         </a>
-        <div class="name">${photographer[i].name}</div>
-        <div class="localisation">${photographer[i].city}, ${photographer[i].country}</div>
-        <div class="slogan">${photographer[i].tagline}</div>
-        <div class="price">${photographer[i].price}€/jour</div>
+        <div class="name">${photographers[i].name}</div>
+        <div class="localisation">${photographers[i].city}, ${photographers[i].country}</div>
+        <div class="slogan">${photographers[i].tagline}</div>
+        <div class="price">${photographers[i].price}€/jour</div>
     `;
 
     //liste pour les tags
@@ -41,7 +66,7 @@ function showPhotograph(jsonObj) {
     myList.classList.add("tags");
 
     // boucle pour les tags
-    var superTags = photographer[i].tags;
+    var superTags = photographers[i].tags;
     for (var j = 0; j < superTags.length; j++) {
       var listItem = document.createElement("li");
       listItem.textContent = "#" + superTags[j];
